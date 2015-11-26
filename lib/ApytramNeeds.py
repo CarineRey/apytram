0a3f64745b9a495e848d0201e83bf027d2cb907e
Carine <carine.rey@ens-lyon.fr>
2015-11-26 18:30:18 +0100

    initialisation

---
+++ lib/ApytramNeeds.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import subprocess
+
+def add_paired_read_names(File):
+    "Add paired read name to a read name list"
+    if os.path.isfile(File):
+        command1 = """awk '{ print $0; if (match($0,"1$")) sub("1$",2,$0); else if (match($0,"2$")) sub("2$",1,$0); print $0}' %s  | sort -u > %s """ %(File, File+".paired")
+        command2 = "mv %s %s" %(File+".paired", File)
+        os.system(command1)
+        os.system(command2)
+    return 0
