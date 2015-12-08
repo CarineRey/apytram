405b00258a03d4723bd1da202d116b25482aec7e
Carine <carine.rey@ens-lyon.fr>
2015-12-08 11:54:39 +0100

    add --finish_all_ietr and -query_pep options

--- apytram.py
+++ apytram.py
@@ -21,16 +21,14 @@ parser = argparse.ArgumentParser(prog = "apytram.py",
                                  description='''
     Run apytram.py on a fastq file to retrieve
     homologous sequences of bait sequences.''')
-
-requiredOptions = parser.add_argument_group('Required arguments')
-requiredOptions.add_argument('-d', '--database', nargs='?', type=str,
-                             help='Database preffix name', required=True)
-requiredOptions.add_argument('-t', '--database_type', type=str, choices=["single","paired"],
-                             help='single or paired end database', required=True)
-
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
 
+requiredOptions = parser.add_argument_group('Required arguments')
+requiredOptions.add_argument('-d', '--database', nargs='?', type=str,
+                             help='Database preffix name. If a database with the same name already exist, the existing database will be kept and the database will NOT be rebuilt.', required=True)
+requiredOptions.add_argument('-dt', '--database_type', type=str, choices=["single","paired"],
+                             help='single or paired end RNA-seq data', required=True)
 
 
 
@@ -39,9 +37,12 @@ InOutOptions = parser.add_argument_group('Input and Output Files')
 InOutOptions.add_argument('-fa', '--fasta',  type=str,
                    help = "Fasta formatted RNA-seq data to build the database of reads")
 InOutOptions.add_argument('-fq', '--fastq',  type=str,
-                   help = "Fastq formatted RNA-seq data to build the database of reads")
+                   help = "Fastq formatted RNA-seq data to build the database of reads. (The fastq will be first convert in fasta file")
 InOutOptions.add_argument('-q', '--query',  type=str,
-                    help = "Fasta file (nt) with bait sequence for the apytram run." )
+                    help = "Fasta file (nucl) with bait sequence for the apytram run. If no query is submitted, the database will be only build." )
+InOutOptions.add_argument('-pep', '--query_pep',  type=str,
+                   default = "",       
+                   help = "Fasta file containing the query in the peptide format. It will be used at the first iteration as bait sequences to fish reads. It must be accompany of the query in nucleotide format (-q option)")
 InOutOptions.add_argument('-out', '--output_preffix',  type=str, default = "./apytram",
                    help = "Output preffix (Default ./apytram)")
 InOutOptions.add_argument('-tmp',  type=str,
@@ -53,9 +54,8 @@ InOutOptions.add_argument('--stats', action='store_true',
                              help='Create files with statistics on each iteration')
 InOutOptions.add_argument('--plot', action='store_true',
                              help='Create file with plot on statistics on each iteration')
-InOutOptions.add_argument('-log', nargs='?', type=str, default="apytram.log",
-                   help = "a log file to report avancement (default: apytram.log)"
-                   )
+InOutOptions.add_argument('-log', type=str, default="apytram.log",
+                   help = "a log file to report avancement (default: apytram.log)")
 
 
 SearchOptions = parser.add_argument_group('Arguments for search thresholds')
@@ -63,27 +63,40 @@ SearchOptions = parser.add_argument_group('Arguments for search thresholds')
 SearchOptions.add_argument('-i', '--iteration_max',  type=int,
                     help = "Maximum number of iteration. (Default 5)",
                     default = 5 )
+InOutOptions.add_argument('--finish_all_iter',  action='store_true',
+                    default = False,
+                    help = "apytram will finish all iteration (-i) even if there is no improvment) (default: False)")
 SearchOptions.add_argument('-e', '--evalue',  type=float,
-                    help = "Evalue. (Default 1e-3)",
+                    help = "Evalue threshold of the blastn of the bait queries on the database of reads . (Default 1e-3)",
                     default = 1e-3 )
 SearchOptions.add_argument('--required_coverage',  type=float,
                     help = "Required coverage of a bait sequence to stop iteration (Default: No threshold)",
                     default = 200 )
-
 SearchOptions.add_argument('-id', '--min_id',  type=int,
-                    help = "Minimum identity percentage with a query to keep a sequence  (Default 50)",
-                    default = 50 )
+                    help = "Minimum identity percentage with a query to keep a sequence at the end of a iteration (Default 20)",
+                    default = 20 )
 SearchOptions.add_argument('-l', '--min_len',  type=int,
-                    help = "Minimum length to keep a sequence  (Default 200)",
+                    help = "Minimum length to keep a sequence at the end of a iteration (Default 200)",
                     default = 200 )
 
 MiscellaneousOptions = parser.add_argument_group('Miscellaneous options')
 MiscellaneousOptions.add_argument('--threads',  type=int,
-                    help = "Available threads. (Default 1)",
+                    help = "Number of available threads. (Default 1)",
                     default = 1 )
 
 ### Option parsing
 args = parser.parse_args()
+
+## Example for the dev
+#args = parser.parse_args(''' -d example_exec/db/examplefq
+#                             -out Out_test/apytram
+#                             -fq example/example_db.fastq
+#                            -q example/ref_gene.fasta 
+#                             --database_type paired
+#                             --keep_iterations
+#                             --plot
+#                             -i 5'''.split())
+
 
 ### Arguments reading
 MaxIteration = args.iteration_max
@@ -93,14 +106,17 @@ MinIdentityPercentage = args.min_id
 MinLength = args.min_len
 KeepIterations = args.keep_iterations
 RequiredCoverage = args.required_coverage
+FinishAllIter = args.finish_all_iter
 
 if args.database_type == "paired":
     PairedData = True
 else:
     PairedData = False
-
+    
 if args.plot:
     args.stats = True
+    
+
 
 ### Set up the output directory
 if args.log:
@@ -151,6 +167,22 @@ else:
     logger.error("The output preffix must be defined")
     sys.exit(1)
 
+### Check that query files exist
+
+if args.query:
+    if not os.path.isfile(args.query):
+        logger.error(args.query+" (-q) is not a file.")
+        sys.exit(1)
+
+# If the -pep option is used, the -q option must be precise
+if args.query_pep:
+    if not os.path.isfile(args.query_pep):
+        logger.error(args.query_pep+" (-pep) is not a file.")
+        sys.exit(1)
+    
+    if not args.query:
+        logger.error("-pep option must be accompanied of the query in nucleotide format (-q option)")
+        sys.exit(1)
 
 ### Check that there is a database else built it
 DatabaseName = args.database
@@ -259,9 +291,15 @@ while (i < MaxIteration) and (Stop == False):
     logger.info("Blast bait sequences on reads database")
     start_blast_time = time.time()
     ReadNamesFile = "%s/ReadNames.%d.txt" % (TmpDirName,i)
-    BlastnProcess = BlastPlus.Blast("blastn", DatabaseName, BaitSequences)
-    BlastnProcess.Evalue = Evalue
-    BlastnProcess.Task = "blastn"
+
+    if args.query_pep and i == 1:
+        BlastnProcess = BlastPlus.Blast("tblastn", DatabaseName, args.query_pep)
+        BlastnProcess.Evalue = Evalue
+    else:
+        BlastnProcess = BlastPlus.Blast("blastn", DatabaseName, BaitSequences)
+        BlastnProcess.Evalue = Evalue
+        BlastnProcess.Task = "blastn"
+
     BlastnProcess.Threads = Threads
     BlastnProcess.OutFormat = "6 sacc"
     # Write read names in ReadNamesFile
@@ -269,12 +307,15 @@ while (i < MaxIteration) and (Stop == False):
     StatsDict["iter_%d" %(i)]["BlastTime"] = time.time() - start_blast_time
     logger.debug("blast --- %s seconds ---" % (StatsDict["iter_%d" %(i)]["BlastTime"]))
     if PairedData:
-        # Get paired reads names
+        # Get paired reads names and remove duplicated names
         ExitCode = ApytramNeeds.add_paired_read_names(ReadNamesFile)
+    else:
+        # Remove duplicated names
+        ExitCode = ApytramNeeds.remove_duplicated_read_names(ReadNamesFile)
         
     # Compare the read list names with the list of the previous iteration:
     Identical = ApytramNeeds.are_identical(ReadNamesFile,"%s/ReadNames.%d.txt" % (TmpDirName,i-1))
-    if Identical:
+    if Identical and not FinishAllIter:
         logger.info("Reads from the current iteration are identical than the previous")
         Stop = True
         IterationNotFinished = True
@@ -369,7 +410,7 @@ while (i < MaxIteration) and (Stop == False):
                 ExonerateProcess.Ryo = "%ti\t%qi\t%ql\t%qal\t%tal\t%tl\t%pi\n"
                 ExonerateResult = ExonerateProcess.get_output()
                 AlmostIdenticalResults = ApytramNeeds.check_almost_identical_exonerate_results(ExonerateResult)
-                if AlmostIdenticalResults:
+                if AlmostIdenticalResults and not FinishAllIter:
                     logger.info("Contigs are almost identical than the previous iteration (Same size (~98%), > 99% identity)")
                     Stop =True
                 StatsDict["iter_%d" %(i)]["Exonerate2Time"] = time.time() - start_exo_time
@@ -386,24 +427,24 @@ while (i < MaxIteration) and (Stop == False):
             MafftProcess.AddOption = FileteredTrinityFasta
             MafftResult = MafftProcess.get_output()
             StatsDict["iter_%d" %(i)]["StrictCoverage"], StatsDict["iter_%d" %(i)]["LargeCoverage"] = ApytramNeeds.calculate_coverage(MafftResult)
-            logger.info("Strict Coverage: %d\tLarge Coverage: %d" %(StatsDict["iter_%d" %(i)]["StrictCoverage"], StatsDict["iter_%d" %(i)]["LargeCoverage"]))
+            logger.info("Strict Coverage: %s\tLarge Coverage: %s" %(StatsDict["iter_%d" %(i)]["StrictCoverage"], StatsDict["iter_%d" %(i)]["LargeCoverage"]))
             StatsDict["iter_%d" %(i)]["MafftTime"] = time.time() - start_mafft_time
             logger.debug("mafft --- %s seconds ---" % (StatsDict["iter_%d" %(i)]["MafftTime"]))
             
-            # Stop iteration if the Largecoverage is not improved and also the Total length
-            
-            if StatsDict["iter_%d" %(i)]["TotalLength"] > StatsDict["iter_%d" %(i-1)]["TotalLength"]:
-                pass
-            elif StatsDict["iter_%d" %(i)]["TotalScore"] > StatsDict["iter_%d" %(i-1)]["TotalScore"]:
-                pass
-            elif StatsDict["iter_%d" %(i)]["LargeCoverage"] <= StatsDict["iter_%d" %(i-1)]["LargeCoverage"]:
-                logger.info("This iteration have a large coverage inferior (or equal) to the previous iteration")
-                Stop = True
-            
-            # Stop iteration if the RequiredCoverage is attained
-            if StatsDict["iter_%d" %(i)]["LargeCoverage"] >= RequiredCoverage:
-                logger.info("This iteration attains the required bait sequence coverage (%d >= %d)" % (StrictCoverage,RequiredCoverage))
-                Stop = True
+            if not FinishAllIter:
+                # Stop iteration if the Largecoverage is not improved and also the Total length
+                if StatsDict["iter_%d" %(i)]["TotalLength"] > StatsDict["iter_%d" %(i-1)]["TotalLength"]:
+                    pass
+                elif StatsDict["iter_%d" %(i)]["TotalScore"] > StatsDict["iter_%d" %(i-1)]["TotalScore"]:
+                    pass
+                elif StatsDict["iter_%d" %(i)]["LargeCoverage"] <= StatsDict["iter_%d" %(i-1)]["LargeCoverage"]:
+                    logger.info("This iteration have a large coverage inferior (or equal) to the previous iteration")
+                    Stop = True
+
+                # Stop iteration if the RequiredCoverage is attained
+                if StatsDict["iter_%d" %(i)]["LargeCoverage"] >= RequiredCoverage:
+                    logger.info("This iteration attains the required bait sequence coverage (%d >= %d)" % (StrictCoverage,RequiredCoverage))
+                    Stop = True
 
             ### Write a fasta file for this iteration if tere is the option --keep_iterations
             
@@ -412,8 +453,8 @@ while (i < MaxIteration) and (Stop == False):
                 ExitCode = ApytramNeeds.write_apytram_output(FileteredTrinityFasta, TrinityExonerateResultsDict,
                                                  "%s.iter_%d.best.fasta" %(OutPreffixName,i), 
                                                  Header = TrinityExonerateProcess.Ryo.replace('%',"").replace("\n","").split(),
-                                                 Names = BestScoreNames,
-                                                 Message = "iter_%d." %i)
+                                                 Names = BestScoreNames.values(),
+                                                 Message = "iter_%d.best." %i)
                 # All sequences of the iteration
                 ExitCode = ApytramNeeds.write_apytram_output(FileteredTrinityFasta,
                                                  TrinityExonerateResultsDict,
@@ -446,7 +487,8 @@ if i: #We check that there is at least one iteration with a result
     ExitCode = ApytramNeeds.write_apytram_output(FileteredTrinityFasta, TrinityExonerateResultsDict,
                                                  OutPreffixName+".best.fasta", 
                                                  Header = TrinityExonerateProcess.Ryo.replace('%',"").replace("\n","").split(),
-                                                 Names = BestScoreNames)
+                                                 Names = BestScoreNames.values(),
+                                                 Message = ".best." %i)
     # Last iteration
     ExitCode = ApytramNeeds.write_apytram_output(FileteredTrinityFasta,
                                                  TrinityExonerateResultsDict,
