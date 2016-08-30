004849f4856d10dd0ecd51982f7573361027f52a
Carine <carine.rey@ens-lyon.fr>
2016-08-30 14:47:05 +0200

    code cleaning using pylint

--- lib/Aligner.py
+++ lib/Aligner.py
@@ -4,22 +4,22 @@
 # File: Aligner.py
 # Created by: Carine Rey
 # Created on: Nov 2015
-# 
-# 
+#
+#
 # Copyright or © or Copr. Carine Rey
-# This software is a computer program whose purpose is to assembly 
+# This software is a computer program whose purpose is to assembly
 # sequences from RNA-Seq data (paired-end or single-end) using one or
-# more reference homologous sequences. 
+# more reference homologous sequences.
 # This software is governed by the CeCILL license under French law and
-# abiding by the rules of distribution of free software.  You can  use, 
+# abiding by the rules of distribution of free software.  You can  use,
 # modify and/ or redistribute the software under the terms of the CeCILL
 # license as circulated by CEA, CNRS and INRIA at the following URL
-# "http://www.cecill.info". 
+# "http://www.cecill.info".
 # As a counterpart to the access to the source code and  rights to copy,
 # modify and redistribute granted by the license, users are provided only
 # with a limited warranty  and the software's author,  the holder of the
 # economic rights,  and the successive licensors  have only  limited
-# liability. 
+# liability.
 # In this respect, the user's attention is drawn to the risks associated
 # with loading,  using,  modifying and/or developing or reproducing the
 # software by the user in light of its specific status of free software,
@@ -27,19 +27,17 @@
 # therefore means  that it is reserved for developers  and  experienced
 # professionals having in-depth computer knowledge. Users are therefore
 # encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
-# same conditions as regards security. 
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
 import logging
 import os
-import sys
 import subprocess
 
-
-class Exonerate:
+class Exonerate(object):
     """Define an object to launch Exonerate"""
     def __init__(self, TargetFile, QueryFile):
         self.logger = logging.getLogger('apytram.lib.Aligner.Exonerate')
@@ -54,22 +52,22 @@ class Exonerate:
         self.Exhaustive = False
 
     def get_output(self):
-        Out = "Command line: []\nHostname:\n-- completed exonerate analysis"
-        command = ["exonerate","-t", self.TargetFile,"-q", self.QueryFile,
+        #Out = "Command line: []\nHostname:\n-- completed exonerate analysis"
+        command = ["exonerate", "-t", self.TargetFile, "-q", self.QueryFile,
                    "--showalignment", self.ShowAlignment,
                    "--showvulgar", self.ShowVulgar]
-        
+
         if self.Model:
-            command.extend(["--model",str(self.Model)])
+            command.extend(["--model", str(self.Model)])
         if self.Bestn > 0:
-            command.extend(["--bestn",str(self.Bestn)])
+            command.extend(["--bestn", str(self.Bestn)])
         if self.Identity > 0:
-            command.extend(["--percent",str(self.Identity)])
+            command.extend(["--percent", str(self.Identity)])
         if self.Ryo:
-            command.extend(["--ryo",str(self.Ryo)])
+            command.extend(["--ryo", str(self.Ryo)])
         if self.Exhaustive:
-            command.extend(["--exhaustive","T"])
-        
+            command.extend(["--exhaustive", "T"])
+
         self.logger.debug(" ".join(command))
         p = subprocess.Popen(command,
                                     stdout=subprocess.PIPE,
@@ -78,20 +76,20 @@ class Exonerate:
         if err:
             self.logger.error("Unexpected error when we launch Exonerate:\n")
             self.logger.error(err)
-            return (out,err,"")
-            
+            return (out, err, "")
+
         # Remove Exonerate default lines (First, second (which contains Hostname) and last):
         List = out.strip().split("\n")
         Start = 0
         while not "Hostname" in List[Start]:
             Start += 1
-            
+
         ExonerateResults = "\n".join(List[Start+1:-1])
-        
-        return (out,err,ExonerateResults)
+
+        return (out, err, ExonerateResults)
 
 
-class Mafft:
+class Mafft(object):
     """Define an object to launch Mafft"""
     def __init__(self, InputFile):
         self.logger = logging.getLogger('apytram.lib.Aligner.Mafft')
@@ -100,7 +98,7 @@ class Mafft:
         self.AdjustdirectionOption = False
         self.AutoOption = False
         self.QuietOption = False
-        
+
     def get_output(self):
         command = ["mafft"]
 
@@ -110,22 +108,19 @@ class Mafft:
             command.append("--auto")
         if self.AddOption:
             if os.path.isfile(self.AddOption):
-                command.extend(["--add",self.AddOption])
+                command.extend(["--add", self.AddOption])
         if self.QuietOption:
             command.append("--quiet")
-        
+
         command.append(self.InputFile)
         self.logger.debug(" ".join(command))
         p = subprocess.Popen(command,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
         (out, err) = p.communicate()
         if err:
             self.logger.error("Unexpected error when we launch Mafft:\n")
             self.logger.error(err)
-            out = ""   
-           
-        return (out,err)
+            out = ""
 
-
-    
+        return (out, err)
--- lib/Trinity.py
+++ lib/Trinity.py
@@ -4,22 +4,22 @@
 # File: Trinity.py
 # Created by: Carine Rey
 # Created on: Nov 2015
-# 
-# 
+#
+#
 # Copyright or © or Copr. Carine Rey
-# This software is a computer program whose purpose is to assembly 
+# This software is a computer program whose purpose is to assembly
 # sequences from RNA-Seq data (paired-end or single-end) using one or
-# more reference homologous sequences. 
+# more reference homologous sequences.
 # This software is governed by the CeCILL license under French law and
-# abiding by the rules of distribution of free software.  You can  use, 
+# abiding by the rules of distribution of free software.  You can  use,
 # modify and/ or redistribute the software under the terms of the CeCILL
 # license as circulated by CEA, CNRS and INRIA at the following URL
-# "http://www.cecill.info". 
+# "http://www.cecill.info".
 # As a counterpart to the access to the source code and  rights to copy,
 # modify and redistribute granted by the license, users are provided only
 # with a limited warranty  and the software's author,  the holder of the
 # economic rights,  and the successive licensors  have only  limited
-# liability. 
+# liability.
 # In this respect, the user's attention is drawn to the risks associated
 # with loading,  using,  modifying and/or developing or reproducing the
 # software by the user in light of its specific status of free software,
@@ -27,21 +27,19 @@
 # therefore means  that it is reserved for developers  and  experienced
 # professionals having in-depth computer knowledge. Users are therefore
 # encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
-# same conditions as regards security. 
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
-import os
-import sys
 import logging
 import subprocess
 
 
-class Trinity:
+class Trinity(object):
     """Define an object to launch Trinity"""
-    def __init__(self, OutputFile, single = "", left = "", right = ""):
+    def __init__(self, OutputFile, single="", left="", right=""):
         self.logger = logging.getLogger('apytram.lib.Trinity')
         self.seqType = "fa"
         self.max_memory = 1
@@ -56,51 +54,53 @@ class Trinity:
         self.NoPathMerging = False
         self.NormalizeReads = False
         self.SS_lib_type = ""
-    
+
     def launch(self):
-        ExitCode = 0
-        command = ["Trinity","--no_version_check","--seqType", self.seqType,
+        command = ["Trinity", "--no_version_check",
+                   "--seqType", self.seqType,
                    "--output", self.OutputFile,
-                   "--CPU", str(self.CPU), "--max_memory", str(int(self.max_memory))+"G"]
-       
+                   "--CPU", str(self.CPU),
+                   "--max_memory", str(int(self.max_memory))+"G"]
+
         if self.FullCleanup:
             command.append("--full_cleanup")
-            
+
         if self.MinLength != 200:
-            command.extend(["--min_contig_length",str(self.MinLength)])
-            
+            command.extend(["--min_contig_length", str(self.MinLength)])
+
         if self.SingleInputFile:
-            command.extend(["--single",self.SingleInputFile])
-            
+            command.extend(["--single", self.SingleInputFile])
+
         if self.RightInputFile:
-            command.extend(["--right",self.RightInputFile])  
-            
+            command.extend(["--right", self.RightInputFile])
+
         if self.LeftInputFile:
-            command.extend(["--left",self.LeftInputFile])  
-            
+            command.extend(["--left", self.LeftInputFile])
+
         if self.RunAsPaired:
             command.append("--run_as_paired")
-        
+
         if self.NoPathMerging:
             command.append("--no_path_merging")
-            
+
         if self.NormalizeReads:
             command.append("--normalize_reads")
-            
-        if self.SS_lib_type in ["FR","RF","F","R"]:
-            command.extend(["--SS_lib_type",self.SS_lib_type])
-        
+
+        if self.SS_lib_type in ["FR", "RF", "F", "R"]:
+            command.extend(["--SS_lib_type", self.SS_lib_type])
+
         self.logger.debug(" ".join(command))
         p = subprocess.Popen(command,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-        out, err = p.communicate()
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        (out, err) = p.communicate()
         returncode = p.poll()
-        
+
         if err:
-            self.logger.error("Unexpected error when we launch Trinity:\n")
-            self.logger.debug("[...]\n"+"\n".join(out.strip().split("\n")[-20:]))
+            self.logger.error(
+                 "Unexpected error when we launch Trinity:\n")
+            self.logger.debug(
+                 "[...]\n"+"\n".join(out.strip().split("\n")[-20:]))
             self.logger.error(err)
-                  
-        return (out,err,returncode)
-    
+
+        return (out, err, returncode)
