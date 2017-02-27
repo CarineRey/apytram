40f8dffda8f88c5d84cbe97840d2fb88be25fb77
Carine <carine.rey@ens-lyon.fr>
2017-02-27 13:30:31 +0100

    add NGM library

---
+++ ApytramLib/Ngm.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# File: Ngm.py
+# Created by: Carine Rey
+# Created on: Feb 2017
+#
+#
+# Copyright or © or Copr. Carine Rey
+# This software is a computer program whose purpose is to assembly
+# sequences from RNA-Seq data (paired-end or single-end) using one or
+# more reference homologous sequences.
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+import os
+import logging
+import subprocess
+
+
+class Ngm(object):
+    """Define an object to create a ngm instance"""
+    def __init__(self, reference, query, sam="", output_readnames=""):
+        self.logger = logging.getLogger('apytram.lib.Ngm.ngm')
+        self.reference = reference
+        self.query = query
+        
+        
+        self.sensitivity = 1      # --threads
+        self.threads = 1          # --sensitivity
+        self.min_identity = 0     #-i/--min-identity
+        self.min_residues = 0     #-R/--min-residues
+        self.min_mq = 0           #-Q/--min-mq
+        self.fast_pairing = False # --fast-pairing
+
+
+        self.no_unal = True        #--no-unal
+        self.sam = sam             #--output
+        self.output_readnames = output_readnames
+
+
+    def launch(self):
+        command = ["ngm", "-r", self.reference, "-q", self.query, "--no-progress", "--skip-save"]
+
+        if self.threads != 1:
+            command.extend(["-t", str(self.threads)])
+        if self.sensitivity != 1:
+            command.extend(["--sensitivity", str(self.sensitivity)])
+        if self.min_identity != 0:
+            command.extend(["--min-identity", str(self.min_identity)])
+        if self.min_residues != 0:
+            command.extend(["--min-residues", str(self.min_residues)])
+        if self.min_mq != 0:
+            command.extend(["--min-mq", str(self.min_mq)])
+        if self.no_unal:
+            command.extend(["--no-unal"])
+            
+        
+        if self.sam:
+            command.extend(["--output", self.sam])
+            self.logger.debug(" ".join(command))
+            p = subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+            (out, err) = p.communicate()
+        
+        elif self.output_readnames:
+            self.logger.debug(" ".join(command) + """ | cut -f 1 | grep -v -e "^@" """)
+            p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)                    
+            p1 = subprocess.Popen(["cut", "-f", "1"], stdin=p.stdout, stdout = subprocess.PIPE)
+            with open(self.output_readnames, 'w') as OUTPUTFILE:
+                p2 = subprocess.Popen(["grep", "-v", "^@"], stdin=p1.stdout, stdout = OUTPUTFILE)
+            
+            p1.wait()
+            p1.stdout.close()
+            p2.wait()
+            (out, err) = p.communicate()
+        
+        if os.path.isfile(self.output_readnames) and (os.stat(self.output_readnames).st_size == 0):
+            if err:
+                self.logger.error(err)
+        return (out, err)
+
